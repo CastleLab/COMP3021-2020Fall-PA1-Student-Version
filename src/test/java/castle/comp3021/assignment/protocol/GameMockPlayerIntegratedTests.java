8287119c9f8d745f792d0bea69d66415725fda16
Aaron <troublor@live.com>
2020-09-17 18:37:15 +0800

    fix: issue on tests for winning condition. MockPiece should not win by leaving central place

--- src/test/java/castle/comp3021/assignment/protocol/GameMockPlayerIntegratedTests.java
+++ src/test/java/castle/comp3021/assignment/protocol/GameMockPlayerIntegratedTests.java
@@ -1,8 +1,8 @@
 package castle.comp3021.assignment.protocol;
 
 import castle.comp3021.assignment.JesonMor;
-import castle.comp3021.assignment.mock.MockPiece;
 import castle.comp3021.assignment.mock.MockPlayer;
+import castle.comp3021.assignment.piece.Knight;
 import castle.comp3021.assignment.util.SampleTest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -32,24 +32,27 @@ public class GameMockPlayerIntegratedTests {
     @Test
     @SampleTest
     public void testCentralPlaceWin() {
-        this.config.addInitialPiece(new MockPiece(player1), 0, 0);
-        this.config.addInitialPiece(new MockPiece(player2), 2, 2);
+        var config = this.config = new Configuration(5, new Player[]{player1, player2});
+        config.addInitialPiece(new Knight(player1), 0, 0);
+        config.addInitialPiece(new Knight(player2), 4, 4);
         player1.setNextMoves(new Move[]{
-                new Move(0, 0, 0, 1),
-                new Move(0, 1, 1, 1),
-                new Move(1, 1, 1, 0),
+                new Move(0, 0, 2, 1),
+                new Move(2, 1, 4, 2),
+                new Move(4, 2, 3, 0),
+                new Move(3, 0, 2, 2),
+                new Move(2, 2, 1, 4),
         });
         player2.setNextMoves(new Move[]{
-                new Move(2, 2, 2, 1),
-                new Move(2, 1, 2, 2),
-                new Move(2, 2, 1, 2),
-                new Move(1, 2, 2, 2),
+                new Move(4, 4, 2, 3),
+                new Move(2, 3, 4, 4),
+                new Move(4, 4, 2, 3),
+                new Move(2, 3, 4, 4),
         });
         var game = new JesonMor(this.config);
         var winner = game.start();
         assertEquals(player1, winner);
-        assertEquals(5, game.getNumMoves());
-        assertEquals(3, player1.getScore());
-        assertEquals(2, player2.getScore());
+        assertEquals(9, game.getNumMoves());
+        assertEquals(15, player1.getScore());
+        assertEquals(12, player2.getScore());
     }
 }
